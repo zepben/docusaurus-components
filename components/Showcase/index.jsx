fd24aee53ee21319afc4496464f5a107de6c77fd
Alex Lourie <alex.lourie@zepben.com>
2025-12-01 14:55:34 +1030

    [OPS-506] Improvements to ShowCase component (#7)
    Improvements to ShowCase component
    
    - Add optional wrap parameter to wrap the ShowCase in <Layout> (will be used by Evolve/Ednar main docs page)
    - Add a helper ButtonedLink component (used by ShowCase cards to present links)
    - Add optional api and repoUrl buttons (used by Evolve/Ednar main docs page)
    
    Signed-off-by: Alex Lourie <alex.lourie@zepben.com>

--- components/Showcase/index.jsx
+++ components/Showcase/index.jsx
@@ -13,64 +13,104 @@ import Layout from "@theme/Layout";
 import clsx from "clsx";
 import styles from "./styles.module.css";
 import groupBy from "lodash/groupBy";
-import Link from '@docusaurus/Link';
+import {FaGithub} from "react-icons/fa";
+
+type ButtonedLinkParams = {
+    href: string;
+    ui: string;
+};
 
 type Item = {
     title: string;
     description: string;
     siteRelUrl?: string;
+    repoUrl?: string;
+    apiUrl?: string;
     type: string;
 };
 
 type Props = {
     title: string;
     description: string;
+    wrap?: string;
     items: Item[];
 };
 
-const Ui: FunctionComponent<Props> = ({title, description, items}) => {
-    const groupedByType = groupBy(items, (i) => i.type);
+const ButtonedLink = ({href, ui} : ButtonedLinkParams) => (
+        <a
+            className="button button--small button--secondary button--block"
+            href={href}
+            target="_blank"
+            rel="noreferrer noopener"
+        >
+            {ui}
+        </a>
+);
 
-    return (
-        <main className="container margin-vert--lg">
-            {Object.entries(groupedByType).map(
-                ([type, items]: [string, Item[]]) => {
-                    return (
-                        <>
-                            <div className="text--center margin-bottom--md margin-top--md">
-                                <h1>{type}</h1>
-                            </div>
-                            <div className="row">
-                                {items.map((t) => (
-                                    <div key={t.title} className="col col--6 margin-bottom--lg">
-                                        <div className={clsx("card", styles.showcaseItem)}>
-                                            <div className="card__body">
-                                                <div className="avatar">
-                                                    <div className="avatar__intro margin-left--none">
-                                                        <h4 className="avatar__name">{t.title}</h4>
-                                                        <small className="avatar__subtitle">
-                                                            {t.description}
-                                                        </small>
-                                                    </div>
+const MainContent = ({ items }: Props) => (
+    <main className="container margin-vert--lg">
+        {Object.entries(groupBy(items, (i) => i.type)).map(
+            ([type, items]: [string, Item[]]) => {
+                return (
+                    <>
+                        <div className="text--center margin-bottom--md margin-top--md">
+                            <h1>{type}</h1>
+                        </div>
+                        <div className="row">
+                            {items.map((t) => (
+                                <div key={t.title} className="col col--6 margin-bottom--lg">
+                                    <div className={clsx("card", styles.showcaseItem)}>
+                                        <div className="card__body">
+                                            <div className="avatar">
+                                                <div className="avatar__intro margin-left--none">
+                                                    <h4 className="avatar__name">{t.title}</h4>
+                                                    <small className="avatar__subtitle">
+                                                        {t.description}
+                                                    </small>
                                                 </div>
                                             </div>
-                                            {
-                                                <div className="card__footer">
-                                                    <div className="button-group button-group--block">
-                                                        <Link to={t.siteRelUrl}>Documentation</Link>
-                                                    </div>
-                                                </div>
-                                            }
                                         </div>
+                                        {
+                                            <div className="card__footer">
+                                                <div className="button-group button-group--block">
+                                                    {t.siteRelUrl && (
+                                                        <ButtonedLink href={t.siteRelUrl} ui={"Documentation"} />
+                                                    )}
+                                                    {t.apiUrl && (
+                                                        <ButtonedLink href={t.siteRelUrl} ui={"API"} />
+                                                    )}
+                                                    {t.repoUrl && (
+                                                        <ButtonedLink href={t.siteRelUrl} ui={<FaGithub/>} />
+                                                    )}
+                                                </div>
+                                            </div>
+                                        }
                                     </div>
-                                ))}
-                            </div>
-                        </>
-                    );
-                }
-            )}
-        </main>
-    );
+                                </div>
+                            ))}
+                        </div>
+                    </>
+                );
+            })            
+        }
+    </main>
+);
+
+const Ui: FunctionComponent<Props> = (props: Props) => {
+
+    const {wrap, title, description, items} = props;
+    if (wrap) {
+        return (
+            <Layout title={title} description={description}>
+                < MainContent {...props} />
+            </Layout>
+        )
+    }
+    else {
+        return (
+            < MainContent {...props} />
+        )
+    }
 };
 
 export default Ui;
